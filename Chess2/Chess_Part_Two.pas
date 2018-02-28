ace1e907197ad68e12724d37167d82d1316a6761
Saltuganov Sergey <Saltugan@gmail.com>
2018-02-28 22:36:47 +0300

    Almost Completed
    Got rid of duplicating pieces of code.
    Merging CheckDiagonal and CheckLine is probably possible.

--- Chess2/Chess_Part_Two.pas
+++ Chess2/Chess_Part_Two.pas
@@ -3,123 +3,97 @@ PROGRAM Chess2(INPUT, OUTPUT);
 CONST
   Max = 15;
 
-TYPE
-  Coord=RECORD
-         x: INTEGER;
-         y: INTEGER;
-       END;    
+TYPE    
+  Coord = RECORD
+            x: INTEGER;
+            y: INTEGER;
+          END;    
 
 VAR
   Bishop, Rook, Knight: ARRAY[1..Max] of Coord;
-  Board: ARRAY[1..Max,1..Max] OF CHAR;
-  i, j, M, N, b,r,k,d,e,f,Ci,Cj:INTEGER;
+  Board: ARRAY[1..Max, 1..Max] OF CHAR;
+  i, j, M, N, b, r, k, d, e, f: INTEGER;
   Ch: CHAR;
   FIn: TEXT;
   
 PROCEDURE CheckForKnight();
 BEGIN
-   FOR i:=1 TO M DO
-    FOR j:=1 TO N DO
-      IF (Board[i,j] = '.') THEN
+   FOR i := 1 TO M DO
+    FOR j := 1 TO N DO
+      IF (Board[i, j] = '.') THEN
         FOR d:=1 TO k DO
           BEGIN
             IF ((abs(Knight[d].y - i) = 2) AND (abs(Knight[d].x - j) = 1)) THEN
-              Board[i,j]:= '#';
+              Board[i, j] := '#';
             IF ((abs(Knight[d].y - i) = 1) AND (abs(Knight[d].x - j) = 2)) THEN
-              Board[i,j]:= '#'
+              Board[i, j] := '#'
           END;  
+END;
+
+PROCEDURE CheckLine(VAR dir1, dir2: INTEGER);
+VAR
+  ytemp, xtemp: INTEGER;
+BEGIN
+  xtemp := Rook[e].x;
+  ytemp := Rook[e].y;
+  WHILE ((Board[ytemp + dir1, xtemp + dir2] = '.') OR (Board[ytemp + dir1, xtemp + dir2] = '#')) DO
+    BEGIN
+      Board[ytemp + dir1, xtemp + dir2] := '#';
+      ytemp := ytemp + dir1;
+      xtemp := xtemp + dir2
+    END
 END;
 
 PROCEDURE CheckForRook();
 VAR
-  ytemp, xtemp: INTEGER;
+  dir1, dir2: INTEGER;
 BEGIN
-  FOR e:=1 TO r DO
+  FOR e := 1 TO r DO
     BEGIN
-      FOR i:=1 TO M DO
-        BEGIN
-          IF (Rook[e].y=i) THEN
-            BEGIN
-              ytemp:=Rook[e].y;
-              WHILE ((Board[ytemp-1,Rook[e].x] = '.') OR (Board[ytemp-1,Rook[e].x] = '#')) DO
-                BEGIN
-                  Board[ytemp-1,Rook[e].x] := '#';
-                  ytemp:=ytemp-1
-                END;
-              ytemp:=Rook[e].y;
-              WHILE ((Board[ytemp+1,Rook[e].x] = '.') OR (Board[ytemp+1,Rook[e].x] = '#')) DO
-                BEGIN
-                  Board[ytemp+1,Rook[e].x] := '#';
-                  ytemp:=ytemp+1
-                END;   
-            END    
-        END;
-      FOR j:=1 TO M DO
-        BEGIN
-          IF (Rook[e].x=j) THEN
-            BEGIN
-              xtemp:=Rook[e].x;
-              WHILE ((Board[Rook[e].y,xtemp-1] = '.') OR (Board[Rook[e].y,xtemp-1] = '#')) DO
-                BEGIN
-                  Board[Rook[e].y,xtemp-1] := '#';
-                  xtemp:=xtemp-1
-                END;
-              xtemp:=Rook[e].x;
-              WHILE ((Board[Rook[e].y,xtemp+1] = '.') OR (Board[Rook[e].y,xtemp+1] = '#')) DO
-                BEGIN
-                  Board[Rook[e].y,xtemp+1] := '#';
-                  xtemp:=xtemp+1
-                END;   
-            END    
-        END  
+      dir1 := 0;
+      dir2 := 1;
+      CheckLine(dir1, dir2);
+      dir2 := -1;
+      CheckLine(dir1, dir2);
+      dir1 := 1;
+      dir2 := 0;
+      CheckLine(dir1, dir2);
+      dir1 := -1;
+      CheckLine(dir1, dir2);   
     END
 END;
 
 
-PROCEDURE CheckForBishop();
+PROCEDURE CheckDiagonal(VAR dir1, dir2: INTEGER);
 VAR
   ytemp, xtemp: INTEGER;
 BEGIN
+  xtemp := Bishop[f].x;
+  ytemp := Bishop[f].y;
+  WHILE ((Board[ytemp + dir1, xtemp + dir2] = '.') OR (Board[ytemp + dir1, xtemp + dir2] = '#')) DO
+    BEGIN
+      Board[ytemp + dir1, xtemp + dir2]:='#';
+      xtemp := xtemp + dir2;
+      ytemp := ytemp + dir1;
+    END; 
+END;
+
+PROCEDURE CheckForBishop();
+VAR
+  dir1, dir2: INTEGER;
+BEGIN
   FOR f:=1 TO b DO
-    FOR i:=1 TO M DO
-      FOR j:=1 TO N DO 
-        BEGIN
-          IF (abs(Bishop[f].y-i) = abs(Bishop[f].x-j)) THEN
-            BEGIN
-              xtemp:=Bishop[f].x;
-              ytemp:=Bishop[f].y;
-              WHILE ((Board[ytemp+1,xtemp+1] = '.') OR (Board[ytemp+1,xtemp+1] = '#')) DO
-                BEGIN
-                  Board[ytemp+1,xtemp+1]:='#';
-                  xtemp:=xtemp+1;
-                  ytemp:=ytemp+1;
-                END;
-              xtemp:=Bishop[f].x;
-              ytemp:=Bishop[f].y;
-              WHILE ((Board[ytemp+1,xtemp-1] = '.') OR (Board[ytemp+1,xtemp-1] = '#')) DO
-                BEGIN
-                  Board[ytemp+1,xtemp-1]:='#';
-                  xtemp:=xtemp-1;
-                  ytemp:=ytemp+1;
-                END;
-              xtemp:=Bishop[f].x;
-              ytemp:=Bishop[f].y;
-              WHILE ((Board[ytemp-1,xtemp+1] = '.') OR (Board[ytemp-1,xtemp+1] = '#')) DO
-                BEGIN
-                  Board[ytemp-1,xtemp+1]:='#';
-                  xtemp:=xtemp+1;
-                  ytemp:=ytemp-1;
-                END;
-              xtemp:=Bishop[f].x;
-              ytemp:=Bishop[f].y;
-              WHILE ((Board[ytemp-1,xtemp-1] = '.') OR (Board[ytemp-1,xtemp-1] = '#')) DO
-                BEGIN
-                  Board[ytemp-1,xtemp-1]:='#';
-                  xtemp:=xtemp-1;
-                  ytemp:=ytemp-1;
-                END;      
-            END
-        END
+    BEGIN
+      dir1 := 1; 
+      dir2 := 1;
+      CheckDiagonal(dir1, dir2);
+      dir2 := -1;
+      CheckDiagonal(dir1, dir2);
+      dir1 := -1;
+      CheckDiagonal(dir1, dir2);
+      dir2 := 1;
+      CheckDiagonal(dir1, dir2);
+    END
 END;
 
 
@@ -135,69 +109,64 @@ BEGIN
   k:=0; //Для коней
   
   //Считывание координат всех фигур
-  FOR i:=1 TO M DO
+  FOR i := 1 TO M DO
     BEGIN
-      FOR j:=1 TO N DO
+      FOR j := 1 TO N DO
         BEGIN
           READ(FIn, Board[i, j]);
-          IF Board[i,j] = 'B'
+          IF Board[i, j] = 'B'
           THEN
             BEGIN
-              b:=b+1;
-              Bishop[b].x:=j;
-              Bishop[b].y:=i
+              b := b + 1;
+              Bishop[b].x := j;
+              Bishop[b].y := i
             END;
-          IF Board[i,j] = 'K'
+          IF Board[i, j] = 'K'
           THEN
             BEGIN
-              k:=k+1;
-              Knight[k].x:=j;
-              Knight[k].y:=i
+              k := k + 1;
+              Knight[k].x := j;
+              Knight[k].y := i
             END;
-          IF Board[i,j] = 'R'
+          IF Board[i, j] = 'R'
           THEN
             BEGIN
-              r:=r+1;
-              Rook[r].x:=j;
-              Rook[r].y:=i
+              r := r + 1;
+              Rook[r].x := j;
+              Rook[r].y := i
             END    
         END;      
       READLN(FIn)  
     END;
   {Вывод поля с фигурами, но без отметки клеток под боем}    
-  FOR i:=1 TO M DO
+  FOR i := 1 TO M DO
     BEGIN
-      FOR j:=1 TO N DO
-        WRITE(Board[i,j], ' ');
+      FOR j := 1 TO N DO
+        WRITE(Board[i, j], ' ');
       WRITELN      
     END;
    
-  CheckForKnight;
-  CheckForRook;
-  CheckForBishop;
+  CheckForKnight; //Определение клеток, которые бьют Кони
+  CheckForRook;   //Определение клеток, которые бьют Ладьи
+  CheckForBishop; //Определение клеток, которые бьют Слоны
     
-  
-  
   //Вывод координат слонов    
   FOR i:=1 to b DO
-    WRITE('i=',i,': ',Bishop[i].y,' ',Bishop[i].x,'   ');
+    WRITE('i=', i, ': ', Bishop[i].y, ' ', Bishop[i].x, '   ');
   WRITELN;
   //Вывод координат коней  
   FOR i:=1 to k DO
-    WRITE('i=',i,': ',Knight[i].y,' ',Knight[i].x,'   ');
+    WRITE('i=', i, ': ', Knight[i].y, ' ', Knight[i].x, '   ');
   WRITELN;  
   //Вывод координат ладей
   FOR i:=1 to r DO
-    WRITE('i=',i,': ',Rook[i].y,' ',Rook[i].x,'   ');
+    WRITE('i=', i, ': ', Rook[i].y, ' ', Rook[i].x, '   ');
   WRITELN;        
   {Вывод поля с фигурами и с отмеченными клетками под боем}
   FOR i:=1 TO M DO
     BEGIN
       FOR j:=1 TO N DO
-        WRITE(Board[i,j],' ');
+        WRITE(Board[i, j],' ');
       WRITELN      
     END; 
-  
-      
-    
 END.  
